b594e0138320a2ee77492758f8339ab483abe629
shuuun <shun051212@icloud.com>
2016-06-04 11:11:59 +0900

    syu

--- Hello.cs
+++ Hello.cs
@@ -11,6 +11,7 @@ namespace Program
         static void Main(string[] args)
         {
           Console.WriteLine("Helloworld!");
+          Console.WriteLine("I love oppai")
         }
       }
   }
